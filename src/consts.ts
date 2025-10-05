a86e980e499ee7cca3f7850fc828a0d6b98c920f
Arya Pandit <arya.i.pandit@gmail.com>
2025-10-05 10:17:40 +0530

    About Image Working Fix

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'The Galactic Gateway';
+export const SITE_DESCRIPTION = 'A personal transmission from the edge of the observable universe.';
