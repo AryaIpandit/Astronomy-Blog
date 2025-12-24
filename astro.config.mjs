b70e10789a0d89389f0dd58567f0839ebc033745
Arya Pandit <arya.i.pandit@gmail.com>
2025-12-24 08:12:08 +0530

    Fix Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,3 +3,7 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://aryapandit.in',
+});
